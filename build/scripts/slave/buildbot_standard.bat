ee0ba5a700c64820af897c67784282e72b96af5d
timurrrr <timurrrr@3c3640eb-14a6-2bb8-0b94-e9e73afe33f5>
2013-05-29 10:16:09 +0000

    Add /WX flag

--- build/scripts/slave/buildbot_standard.bat
+++ build/scripts/slave/buildbot_standard.bat
@@ -41,10 +41,11 @@ cd %ASAN_PATH% || goto :DIE
 :: This only compiles, not links.
 del *.pdb *.obj *.lib || goto :DIE
 
+:: /WX <- treat warnings as errors
 :: /MP <- parallel buidling (currently disabled)
 :: /MT <- Multi-Threaded CRT with static linking
 :: /Zi <- generate debug info
-cl /nologo /MT /Zi /I.. /I../../include /c *.cc ../P="" /c *.cc inter../sanitizer_commcc interception/*.cc || goto :DIE
+cl /nologo /WX /MT /Zi /I.. /I../../include /c *.cc ../P="" /c *.cc inter../sanitizer_commcc interception/*.cc || goto :DIE
 lib /nologo /OUT:asan_rtl.lib *.obj || g%ROOT%
 
 echo @@@BUILD_STEP asan test@@@
