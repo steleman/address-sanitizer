3d5dbf4c9349f9939bd8323ae2d196986c69e5de
timurrrr <timurrrr@3c3640eb-14a6-2bb8-0b94-e9e73afe33f5>
2013-08-30 14:02:27 +0000

    Revert the recent Win command changes, will hopefully fix the bot

--- trunk/build/scripts/slave/buildbot_standard.bat
+++ trunk/build/scripts/slave/buildbot_standard.bat
@@ -18,27 +18,10 @@ if NOT "%BUILDBOT_REVISION%" == "" set REV_ARG="-r%BUILDBOT_REVISION%"
 :: call -> because "svn" might be a batch script, ouch
 call svn co http://llvm.org/svn/llvm-project/llvm/trunk llvm %REV_ARG% || goto :DIE
 call svn co http://llvm.org/svn/llvm-project/cfe/trunk llvm/tools/clang %REV_ARG% || goto :DIE
-call svn co http://llvm.org/svn/llvm-project/compiler-rt/trunk llvm/projects/compiler-rt %REV_ARG% || goto :DIE
+call svn co http://llvm.org/svn/llvm-project/compiler-rt/trunk compiler-rt %REV_ARG% || goto :DIE
 call svn co http://address-sanitizer.googlecode.com/svn/trunk/win/tests win_tests || echo "Failed to update Windows tests, alas..."
 
 set ROOT=%cd%
-
-echo @@@BUILD_STEP build asan RTL@@@
-:: TODO(timurrrr): Only needed to build asan_thunk.dll actually.
-:: Still useful to detect build errors early...
-set ASAN_PATH=llvm\projects\compiler-rt\lib\asan
-cd %ASAN_PATH% || goto :DIE
-:: This only compiles, not links.
-del *.pdb *.obj *.lib || goto :DIE
-
-:: /WX <- treat warnings as errors
-:: /W3 <- warnings level 3
-:: /MP <- parallel buidling (currently disabled)
-:: /MT <- Multi-Threaded CRT with static linking
-:: /Zi <- generate debug info
-cl /nologo /WX /W3 /MT /Zi /I.. /I../../include /c *.cc ../P="" /c *.cc inter../sanitizer_commcc interception/*.cc || goto :DIE
-lib /nologo /OUT:asan_rtl.lib *.obj ||l /nologo /WX /W3 /MT /Zi /DASAN_DLL_THUNK /c asan_dll_thunk.cc || goto :DIE
-lib /nologo /OUT:asan_dll_thunk.lib asan_dll_thunk.obj || g%ROOT%
 
 echo @@@BUILD_STEP cmake llvm@@@
 mkdir llvm-build
@@ -52,20 +35,34 @@ cd %ROOT%
 
 :: TODO(timurrrr) echo @@@BUILD_STEP test llvm@@@
 
+echo @@@BUILD_STEP build asan RTL@@@
+set ASAN_PATH=compiler-rt\lib\asan
+cd %ASAN_PATH% || goto :DIE
+:: This only compiles, not links.
+del *.pdb *.obj *.lib || goto :DIE
+
+:: /WX <- treat warnings as errors
+:: /W3 <- warnings level 3
+:: /MP <- parallel buidling (currently disabled)
+:: /MT <- Multi-Threaded CRT with static linking
+:: /Zi <- generate debug info
+cl /nologo /WX /W3 /MT /Zi /I.. /I../../include /c *.cc ../P="" /c *.cc inter../sanitizer_commcc interception/*.cc || goto :DIE
+lib /nologo /OUT:asan_rtl.lib *.obj ||l /nologo /WX /W3 /MT /Zi /DASAN_DLL_THUNK /c asan_dll_thunk.cc || goto :DIE
+lib /nologo /OUT:asan_dll_thunk.lib asan_dll_thunk.obj || g%ROOT%
+
 echo @@@BUILD_STEP asan test@@@
 cd win_tests || goto :DIE
 cd win_tests
 C:\cyg-sgwin\bin\make PLATRM_F="/cygdrive/c/cygwin/bin/rm -f"Tclean || goto :DIE
 cd win_tests
-C:\cyg-sgwin\bin\make PLATFORM=Windows CC=../llvm-build/bi-cl FILECHECK=../llvm-build/bin/Debug/FileCheck CFLAGS="-fsanitize=address"e-k || goto :DIE
+C:\cyg-sgwin\bin\make PLATFORM=Windows CC=../llvm-build/bi-cl FILECHECK=../llvm-build/bin/Debug/FileCheck CFLAGS="-Xclang -fsanitize=address" EXTRA_OBJ=../compiler-rt/lib/asan/asan_rtl.libe-k || goto :DIE
 
 echo @@@BUILD_STEP asan DLL thunk test@@@
 cd dll_tests || goto :DIE
 cd win_tests
 C:\cyg-sgwin\bin\make PLATRM_F="/cygdrive/c/cygwin/bin/rm -f"Tclean || goto :DIE
-c:: TODO(timurrrr): clang-cl should automatically link the needed parts of the RTL.
 cd win_tests
-C:\cyg-sgwin\bin\make PLATFORM=W../llvm-build/bin/Debug/clang-cl FILECHECK=../../llvm-build/bin/Debug/FileCheck CFLAGS="-fsanitize=address"ess-sanHOST_LIBS=../../llvm/projects/compiler-rt/lib/asan/asan_rtl.lib EXTRA_GUEST_LIBS="../../llvm/projects/compiler-rt/lib/asan/asan_dll_thunk.lib" -k || goto :DIE
+C:\cyg-sgwin\bin\make PLATFORM=W../llvm-build/bin/Debug/clang-cl FILECHECK=../../llvm-build/bin/Debug/FileCheck CFLAGS="-Xclang -fsanitize=address" EXTRA_HOST_LIBS=../../compiler-rt/lib/asan/asan_rtl.lib EXTRA_GUEST_LIBS="../../compiler-rt/lib/asan/asan_dll_thunk.lib" -k || goto :DIE
 cd %ROOT%
 
 :: TODO(timurrrr) echo @@@BUILD_STEP @@BUILD_STEP asan _tests@@@
